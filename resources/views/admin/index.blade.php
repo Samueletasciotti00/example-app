b1c570a9edafc5c6bd4f2a1ba86d8b50c6a50b53
Samuele <samueletasciotti@icloud.com>
2024-09-20 15:42:55 +0200

    feat: Route link

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -17,7 +17,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <p>Sei dentro la index di Admin</p>
                 </div>
             </div>
         </div>
