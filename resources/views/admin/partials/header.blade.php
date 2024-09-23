04d3b9e20e283cadee49e7a8ed122a19f86ea7cb
Samuele <samueletasciotti@icloud.com>
2024-09-23 18:01:35 +0200

    feat: dynamic user name

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,15 +1,29 @@
 <header>
     <div class="container-fluid bg-dark d-flex justify-content-between p-3 align-items-center">
-        
+
         <!-- Logo -->
         <div class="logo d-flex justify-content-around">
             <!-- <img src="https://thumbs.dreamstime.com/b/libro-macchina-104465645.jpg" alt=""> -->
-            <a href="{{ route('home') }}">Vai al Sito</a>            
+            <a href="{{ route('home') }}">Vai al Sito</a>
         </div>
 
         <!-- USER -->
         <div class="user">
-            USER
+            <ul class="navbar">
+                @guest
+                <li class="nav-item"><a href="{{ route('login') }}">Login</a></li>
+                <li class="nav-item"><a href="{{ route('register') }}">Registrati</a></li>
+
+                @else
+                <li class="nav-item">
+                    <a href="#">
+                        {{ Auth::user()->name }}
+                    </a>
+                </li>
+
+                @endguest
+            </ul>
+
         </div>
     </div>
 </header>
