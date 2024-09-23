3de390bb2a5adc72e95fbbbbf5f4d47c41b5654d
Samuele <samueletasciotti@icloud.com>
2024-09-23 16:56:58 +0200

    feat: use of SCSS partials

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,1 +1,14 @@
-<header>HEADER</header>
+<header>
+    <div class="container-fluid bg-dark d-flex justify-content-between p-3 align-items-center">
+        
+        <!-- Logo -->
+        <div class="logo">
+            <a href="{{ route('home') }}">Vai al Sito</a>            
+        </div>
+
+        <!-- USER -->
+        <div class="user">
+            USER
+        </div>
+    </div>
+</header>
