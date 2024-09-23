c129d4abe381eb0fb24519c632c8af9a9c3d9758
Samuele <samueletasciotti@icloud.com>
2024-09-23 16:35:29 +0200

    feat: structure of Admin Page

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,6 @@ use App\Http\Controllers\Guest\PageController;
 
 Route::get('/',[PageController::class, 'index'])->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 // Route login
 Route::middleware(['auth', 'verified'])
     ->prefix('admin')
@@ -31,5 +25,17 @@ Route::middleware(['auth', 'verified'])
     ->group(function(){
         Route::get('/', [DashboardController::class, 'index'])->name('home');
     });
+
+
+Route::middleware('auth')
+->prefix('profile')
+->name('profile.')
+->group(function () {
+    Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
 
 require __DIR__.'/auth.php';
