0c41e879d55f72baefd1a5fb7ead4df4cf638f61
Samuele <samueletasciotti@icloud.com>
2024-09-25 14:01:29 +0200

    fix: relation for model Category / Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    // Questa function viene visualizzata come proprietà [$project->category];
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    // Fillable delle colonne
+    protected $fillable = ['title','slug'];
 }
